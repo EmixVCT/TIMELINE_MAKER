ce7421e1cfe120d7828f9863e89aa5fc2de5b5de
maxsv <maxsv@172.16.35.241>
2018-06-01 08:32:14 +0200

    Ajout de la table FenetreTimeline

--- TIMELINE_MAKER/src/Vue/FenetreTimeline.java
+++ TIMELINE_MAKER/src/Vue/FenetreTimeline.java
@@ -1,14 +1,36 @@
 package Vue;
 
-public class FenetreTimeline {
+import java.awt.Color;
+import java.awt.Insets;
 
-	public FenetreTimeline() {
-		// TODO Auto-generated constructor stub
-	}
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	public static void main(String[] args) {
+public class FenetreTimeline extends JFrame {
+
+	public FenetreTimeline(String parTitre) {
+		super(parTitre);
 		
-
-	}
+		JPanel contentPane = new JPanel();
+		
+		setContentPane(contentPane);
+		contentPane.setBackground(new Color(230,67,98));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800,600);
+		setVisible(true);
+		setLocation(200,300);
+		}
+			
+	public static void main(String [] args){ //main
+		
+		new FenetreTimeline("Timeline maker"); //cree la fenetre avec le titre en argument
+		}//ferme main
+	
+	
+	public Insets getInsets (){
+		return new Insets (50,30,15,30) ; // HAUT,GAUCHE,BAS,DROITE
+		}
 
 }
+
+
