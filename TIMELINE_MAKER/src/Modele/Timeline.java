fdac24a3fc8fa42dbac3181610dcc59857615cbf
maxsv <maxsv@192.168.1.74>
2018-06-02 13:31:07 +0200

    Ajout de la premier version de la classe Timeline

--- TIMELINE_MAKER/src/Modele/Timeline.java
+++ TIMELINE_MAKER/src/Modele/Timeline.java
@@ -1,9 +1,46 @@
 package Modele;
 
-public class Timeline {
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
-	public Timeline() {
-		// TODO Auto-generated constructor stub
+import Modele.Date;
+import Modele.Evenement;
+
+public class Timeline {
+	private String chTitre;
+	private Date chDateDebut;
+	private Date chDateFin;
+	private int chPeriode;
+	private String chAdresseFichier;
+	private HashMap<Integer,ArrayList<Evenement>> Hash_Evenements;
+	
+	public Timeline(String parTitre,Date parDebut,Date parFin,int parPeriode,String parLien) {
+		chTitre = parTitre;
+		chDateDebut = parDebut;
+		chDateFin = parFin;
+		chPeriode = parPeriode;
+		chAdresseFichier = parLien;
+		Hash_Evenements = new HashMap<Integer,ArrayList<Evenement>>();
+	}
+	
+	public String toString() {
+		return " ";
+	}
+	
+	public void ajout(Evenement parEvt) {
+		Date date = parEvt.getChDate();
+		int annee = date.getAnnee();
+		if (Hash_Evenements.containsKey(annee)){
+			Hash_Evenements.get(annee).add(parEvt);
+		}
+		else{
+			ArrayList <Evenement> liste = new ArrayList();
+			liste.add(parEvt);
+			Hash_Evenements.put(annee, liste);
+		}
 	}
 
 }
